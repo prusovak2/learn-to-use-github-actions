c4f1b3ec78dfceeac23d976d0d44b0be536d719f
prusovak <katerina.prusova@quanti.cz>
2023-12-28 17:20:41 +0100

    failing test added

--- MyTests/UnitTest1.cs
+++ MyTests/UnitTest1.cs
@@ -6,9 +6,15 @@ namespace MyTests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Success()
     {
         int res = MyClass.GetAnswer();
         res.Should().Be(42);
+    }
+
+    [Fact]
+    public void Fail() 
+    {
+        42.Should().Be(73);
     }
 }
